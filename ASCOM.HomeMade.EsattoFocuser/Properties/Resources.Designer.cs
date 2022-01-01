58d1a8fa3ab44cc9d1e9a8e4e6a91d6c7a2ee586
Cedric Raguenaud <32923629+cedric-r@users.noreply.github.com>
2022-01-01 13:59:53 +0100

    Some traces of NGCAT left.

--- ASCOM.HomeMade.EsattoFocuser/Properties/Resources.Designer.cs
+++ ASCOM.HomeMade.EsattoFocuser/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace ASCOM.HomeMade.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ASCOM.NGCAT.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ASCOM.HomeMade.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
